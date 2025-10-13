b04431f90fdf39b41ba96ce8ffac07acf33e2c56
mithilmehta <mithilmehta15@gmail.com>
2025-10-13 11:46:59 +0530

    day 1: setup react project and tailwind working properly

---
+++ frontend/gig/src/App.js
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white text-4xl font-bold">
+      GigConnect Setup Done ✅
+    </div>
+  );
+}
+export default App;
