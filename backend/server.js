11c47629e66ee09a8dceb013e5d1853204505197
mithilmehta <mithilmehta15@gmail.com>
2025-10-13 12:18:21 +0530

    day 2: connected mongoDB and backend server running successfully

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("GigConnect API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
