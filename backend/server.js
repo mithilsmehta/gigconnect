78fa4edb65d00b758263b2f9ec223536bebe646b
mithilmehta <mithilmehta15@gmail.com>
2025-10-14 19:51:59 +0530

    day 3:implemented react+vite from react only and update backend files

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-connectDB();
+app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("GigConnect API is running...");
-});
+app.get("/", (req, res) => res.send("GigConnect API is running..."));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
